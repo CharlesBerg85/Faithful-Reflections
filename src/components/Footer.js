14efdcfd6d55b744cfaa838533da4f45269481b7
CharlesBerg85 <mr.charlesmberg@gmail.com>
2023-06-30 17:06:25 -0700

    Refactoring
    Also made some changes to CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import { useStoreState } from 'easy-peasy';
+
   const today = new Date();
-const Footer = () => {
+  const Footer = () => {
   const postCount = useStoreState((state) => state.postCount);
-  return (
-    <footer className="Footer">
-        <p>{postCount} Bible Blog Posts</p>   
-    </footer>
-  )
+    return (
+      <footer className="Footer">
+          <p>{postCount} Bible Blog Posts</p>   
+      </footer>
+    )
 }
 
 export default Footer
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from 'react-dom';
 import { StoreProvider } from 'easy-peasy';
-import store from './store';
+import store from './store/store';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
