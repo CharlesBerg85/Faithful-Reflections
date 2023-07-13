27ce29f80ef651851549ec91aa2c85f4587ed890
CharlesBerg85 <mr.charlesmberg@gmail.com>
2023-07-12 23:51:12 -0700

    something Missing

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data/db.json'); // Update the path here
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
 const port = process.env.PORT || 3500;
 
 server.use(middlewares);
 server.use(router);
+server.use(cors());
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
