df6caca6cc055e9b60bc8f3d2609f4a3fdddf454
CharlesBerg85 <mr.charlesmberg@gmail.com>
2023-07-12 20:45:09 -0700

    something Missing

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data/db.json'); // Update the path here
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3500;
 
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  }));
 server.use(router);
 
 server.listen(port, () => {
