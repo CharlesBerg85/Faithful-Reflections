47e1f36803d1c85baff6aff6926fae0ea1418ea0
CharlesBerg85 <mr.charlesmberg@gmail.com>
2023-07-12 19:48:40 -0700

    json-server please be found

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data/db.json'); // Update the path here
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
 
 const port = process.env.PORT || 3500;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+  }));
 server.use(router);
 
 server.listen(port, () => {
