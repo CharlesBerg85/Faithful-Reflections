dce6d8ed6bad840c2e33f9b345065c835a53f609
CharlesBerg85 <mr.charlesmberg@gmail.com>
2023-07-13 20:22:07 -0700

    something Missing

--- src/store/store.js
+++ src/store/store.js
@@ -2,38 +2,71 @@ import { createStore, action, thunk, computed } from "easy-peasy";
 import api from '../api/posts';
 
 export default createStore({
+    // Array to store the posts
     posts: [],
+
+    // Action to set the posts in the state
     setPosts: action((state, payload) => {
         state.posts = [...payload];
     }),
+
+    // State for the post title
     postTitle: '',
+
+    // Action to set the post title in the state
     setPostTitle: action((state, payload) => {
         state.postTitle = payload;
     }),
+
+    // State for the post body
     postBody: '',
+
+    // Action to set the post body in the state
     setPostBody: action((state, payload) => {
         state.postBody = payload;
     }),
+
+    // State for the edited post title
     editTitle: '',
+
+    // Action to set the edited post title in the state
     setEditTitle: action((state, payload) => {
         state.editTitle = payload;
     }),
+
+    // State for the edited post body
     editBody: '',
+
+    // Action to set the edited post body in the state
     setEditBody: action((state, payload) => {
         state.editBody = payload;
     }),
+
+    // State for search query
     search: '',
+
+    // Action to set the search query in the state
     setSearch: action((state, payload) => {
         state.search = payload;
     }),
+
+    // Array to store search results
     searchResults: [],
+
+    // Action to set the search results in the state
     setSearchResults: action((state, payload) => {
         state.searchResults = payload;
     }),
+
+    // Computed property to get the count of posts
     postCount: computed((state) => state.posts.length),
+
+    // Computed property to get a post by ID
     getPostById: computed((state) => {
         return (id) => state.posts.find(post => post.id === parseInt(id));
     }),
+
+    // Thunk to save a new post
     savePost: thunk(async (actions, newPost, helpers) => {
         const { posts } = helpers.getState();
         try {
@@ -45,6 +78,8 @@ export default createStore({
             console.log(`Error: ${err.message}`);
         }
     }),
+
+    // Thunk to delete a post
     deletePost: thunk(async (actions, id, helpers) => {
         const { posts } = helpers.getState();
         try {
@@ -54,6 +89,8 @@ export default createStore({
             console.log(`Error: ${err.message}`);
         }
     }),
+
+    // Thunk to edit a post
     editPost: thunk(async (actions, updatedPost, helpers) => {
         const { posts } = helpers.getState();
         const { id } = updatedPost;
